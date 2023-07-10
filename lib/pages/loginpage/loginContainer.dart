d602a7a3d325b0a6799c58811db059d634e5c7e2
emman2k1725 <emmisaiah25@gmail.com>
2023-07-11 00:10:56 +0800

    Sign up functionality

--- lib/pages/loginpage/loginContainer.dart
+++ lib/pages/loginpage/loginContainer.dart
@@ -117,187 +117,194 @@ class _LoginContainerState extends State<LoginContainer> {
                     padding: const EdgeInsets.only(left: 16.0, right: 16.0),
                     height: 530,
                     color: Colors.white,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              30, 20, 0, 0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.asset(
-                              logo,
-                              width: 100,
-                              fit: BoxFit.fitWidth,
+                    child: Form(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                30, 20, 0, 0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.asset(
+                                logo,
+                                width: 100,
+                                fit: BoxFit.fitWidth,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        // Generated code for this Text Widget...
-                        const Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                          child: Text(
-                            'Sign in',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1.5,
-                              wordSpacing: 2.0,
-                            ),
+                          const SizedBox(
+                            height: 20,
                           ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                          child: Text(
-                            'Begin your journey by signing in to your account',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 12.0,
-                              letterSpacing: 1.5,
-                              wordSpacing: 2.0,
-                            ),
-                          ),
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            hintText: 'Enter your email',
-                            border: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.blue),
-                              borderRadius: BorderRadius.circular(
-                                  20.0), // Set the border radius
-                            ),
-                          ),
-                        ),
-
-                        const SizedBox(height: 16.0),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            hintText: 'Enter your password',
-                            border: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.blue),
-                              borderRadius: BorderRadius.circular(
-                                  20.0), // Set the border radius
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16.0),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.blue,
-                          ),
-                          height: 40,
-                          width: 150,
-                          child: TextButton.icon(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.login_outlined,
-                              color: Colors.white,
-                            ),
-                            label: const Text(
+                          // Generated code for this Text Widget...
+                          const Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
+                            child: Text(
                               'Sign in',
                               style: TextStyle(
-                                color: Colors.white,
+                                color: Colors.blue,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1.5,
+                                wordSpacing: 2.0,
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 16.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.blue,
+                          const Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
+                            child: Text(
+                              'Begin your journey by signing in to your account',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12.0,
+                                letterSpacing: 1.5,
+                                wordSpacing: 2.0,
                               ),
-                              height: 40,
-                              width: 200,
-                              child: ElevatedButton.icon(
-                                onPressed: () {
-                                  // Handle the login with Facebook action
-                                },
-                                icon: Image.asset(
-                                  fbLogo,
-                                  width: 30.0,
-                                  height: 30.0,
-                                ),
-                                label: const Text('Login with Facebook'),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.white,
-                                  onPrimary: Colors.black,
+                            ),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              hintText: 'Enter your email',
+                              border: OutlineInputBorder(
+                                borderSide:
+                                    const BorderSide(color: Colors.blue),
+                                borderRadius: BorderRadius.circular(
+                                    20.0), // Set the border radius
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 16.0),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              hintText: 'Enter your password',
+                              border: OutlineInputBorder(
+                                borderSide:
+                                    const BorderSide(color: Colors.blue),
+                                borderRadius: BorderRadius.circular(
+                                    20.0), // Set the border radius
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.blue,
+                            ),
+                            height: 40,
+                            width: 150,
+                            child: TextButton.icon(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.login_outlined,
+                                color: Colors.white,
+                              ),
+                              label: const Text(
+                                'Sign in',
+                                style: TextStyle(
+                                  color: Colors.white,
                                 ),
                               ),
                             ),
-                            const SizedBox(width: 16.0),
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.blue,
-                              ),
-                              height: 40,
-                              width: 180,
-                              child: ElevatedButton.icon(
-                                onPressed: () {
-                                  // Handle the login with Facebook action
-                                },
-                                icon: Image.asset(
-                                  googleLogo,
-                                  width: 30.0,
-                                  height: 30.0,
-                                ),
-                                label: const Text('Login with Google'),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.white,
-                                  onPrimary: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 16.0),
-                        RichText(
-                          text: TextSpan(
-                            text: 'Dont have an account? ',
-                            style: DefaultTextStyle.of(context).style,
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: 'Sign up',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
+                          ),
+                          const SizedBox(height: 16.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
                                   color: Colors.blue,
                                 ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const SignupPage()));
+                                height: 40,
+                                width: 200,
+                                child: ElevatedButton.icon(
+                                  onPressed: () {
+                                    // Handle the login with Facebook action
                                   },
+                                  icon: Image.asset(
+                                    fbLogo,
+                                    width: 30.0,
+                                    height: 30.0,
+                                  ),
+                                  label: const Text('Login with Facebook'),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.white,
+                                    onPrimary: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 16.0),
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.blue,
+                                ),
+                                height: 40,
+                                width: 180,
+                                child: ElevatedButton.icon(
+                                  onPressed: () {
+                                    // Handle the login with Facebook action
+                                  },
+                                  icon: Image.asset(
+                                    googleLogo,
+                                    width: 30.0,
+                                    height: 30.0,
+                                  ),
+                                  label: const Text('Login with Google'),
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.white,
+                                    onPrimary: Colors.black,
+                                  ),
+                                ),
                               ),
                             ],
                           ),
-                        ),
-
-                        const Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                          child: Text(
-                            '- OR -',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12.0,
-                              letterSpacing: 1.5,
-                              wordSpacing: 2.0,
+                          const SizedBox(height: 16.0),
+                          RichText(
+                            text: TextSpan(
+                              text: 'Dont have an account? ',
+                              style: DefaultTextStyle.of(context).style,
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Sign up',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const SignupPage()));
+                                    },
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                      ],
+
+                          const Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
+                            child: Text(
+                              '- OR -',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                letterSpacing: 1.5,
+                                wordSpacing: 2.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ) // Generated code for this Image Widget...
 
                     //  child: Column(
