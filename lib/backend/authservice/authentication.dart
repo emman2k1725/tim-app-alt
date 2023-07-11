8d34fe94f84073c05fcff49ba4f693decf8985ac
emman2k1725 <emmisaiah25@gmail.com>
2023-07-11 14:07:17 +0800

    sign in and sign up error handling

--- lib/backend/authservice/authentication.dart
+++ lib/backend/authservice/authentication.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:toast/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:tim_app/pages/dashboard_main.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final userProfile = FirebaseFirestore.instance.collection('user_profile');
 String userID = "";
+String errMessage = "An error occured. Try again later";
 
 void registerWithEmailPassword(
     String email,
@@ -34,8 +35,13 @@ void registerWithEmailPassword(
       context,
       MaterialPageRoute(builder: (context) => const DashboardMainScreen()),
     );
-  } catch (e) {
-    print(e.toString());
+  } on FirebaseAuthException catch (e) {
+    debugPrint(e.message);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          content: Text(authErrorHandler(
+              parseFirebaseAuthExceptionMessage(input: e.message)))),
+    );
   }
 }
 
@@ -49,7 +55,65 @@ void loginWithEmailPassword(
       context,
       MaterialPageRoute(builder: (context) => const DashboardMainScreen()),
     );
-  } catch (e) {
-    print(e.toString());
+  } on FirebaseAuthException catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          content: Text(authErrorHandler(
+              parseFirebaseAuthExceptionMessage(input: e.message)))),
+    );
   }
+}
+
+String authErrorHandler(String errorCode) {
+  switch (errorCode) {
+    case "user-not-found":
+      {
+        errMessage = "Invalid email or password";
+        break;
+      }
+    case "wrong-password":
+      {
+        errMessage = "Invalid email or password";
+        break;
+      }
+
+    case "user-disabled":
+      {
+        errMessage = "This account is disabled";
+        break;
+      }
+
+    case "network-request-failed":
+      {
+        errMessage = "Network connection failed. Try again later.";
+        break;
+      }
+    case "email-already-in-use":
+      {
+        errMessage = "Email already exists. Please sign in instead.";
+        break;
+      }
+    default:
+      {
+        errMessage = "An error occured. Try again later";
+      }
+  }
+  return errMessage;
+}
+
+String parseFirebaseAuthExceptionMessage(
+    {String plugin = "auth", required String? input}) {
+  if (input == null) {
+    return "unknown";
+  }
+
+  // https://regexr.com/7en3h
+  String regexPattern = r'(?<=\(' + plugin + r'/)(.*?)(?=\)\.)';
+  RegExp regExp = RegExp(regexPattern);
+  Match? match = regExp.firstMatch(input);
+  if (match != null) {
+    return match.group(0)!;
+  }
+
+  return "unknown";
 }
