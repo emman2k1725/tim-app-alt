7f3c541a7446d06847ff22cf7e362879648d131a
Nicole Kaye Cardel <nkcardel@gmail.com>
2023-07-13 22:41:23 +0800

    added dependency on pubspec

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -26,16 +26,16 @@ class _HomeState extends State<Home> {
             image: AssetImage(mainBg),
           ),
         ),
-        child: const SingleChildScrollView(
-              child: Column(
-                children: [
-                  const NavBar(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Container1(),
-                  ImageCarousel(),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const NavBar(),
+              const SizedBox(
+                height: 20,
+              ),
+              const Container1(),
+              ImageCarousel(),
+            ],
           ),
         ),
       ),
