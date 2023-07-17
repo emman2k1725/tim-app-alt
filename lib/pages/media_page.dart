8c1db948a6d8826e0fb79b6458126523ef8205c0
Nicole Kaye Cardel <nkcardel@gmail.com>
2023-07-17 23:47:29 +0800

    minor route change + added media dart file

---
+++ lib/pages/media_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+import '../widgets/navBar.dart';
+
+class MediaPage extends StatefulWidget {
+  const MediaPage({super.key});
+
+  @override
+  State<MediaPage> createState() => _MediaPageState();
+}
+
+class _MediaPageState extends State<MediaPage> {
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        width: w,
+        height: h,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(mainBg),
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const NavBar(),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
