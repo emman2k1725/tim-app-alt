b463532e674baa56dc8afd81e0f2fd93aafb6850
Nicole Kaye Cardel <nkcardel@gmail.com>
2023-07-21 22:08:05 +0800

    media & navbar submenu navigation

--- lib/pages/containers/media_container1.dart
+++ lib/pages/containers/media_container1.dart
@@ -19,7 +19,9 @@ class _MediaContainer1State extends State<MediaContainer1> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: w! / 10, vertical: 5),
+      margin: EdgeInsets.symmetric(
+          horizontal: Responsive.isDesktop(context) ? w! / 10 : w! / 20,
+          vertical: 5),
       child: Responsive.isDesktop(context)
           ? desktopContainer1()
           : mobileContainer1(),
@@ -170,7 +172,7 @@ class _MediaContainer1State extends State<MediaContainer1> {
                           ),
                         );
                       },
-                    )
+                    ),
                   ],
                 )),
           ],
@@ -185,27 +187,141 @@ class _MediaContainer1State extends State<MediaContainer1> {
   //================ MOBILE ===============
   Widget mobileContainer1() {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: w! / 10, vertical: 5),
       child: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(
-              height: h! / 3,
-              width: w! / 2,
-              child: const Padding(
-                padding: EdgeInsets.only(bottom: 30),
-                child: Image(
-                  image: AssetImage(homepageTim),
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            //const SizedBox(height: 30),
+            //  Container(
+            //       height: h!,
+            //       width: w! / 150,
+            //       decoration: const BoxDecoration(
+            //           image: DecorationImage(
+            //               image: AssetImage(mediaImg), fit: BoxFit.cover))),
             Center(
               child: Text(
                 'Media',
                 style: AppTheme.getSecondaryTextStyle(30.0),
               ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: h! / 2,
+              width: w! / 1,
+              child: const Padding(
+                padding: EdgeInsets.only(bottom: 30),
+                child: Image(
+                  image: AssetImage(mediaImg),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Lorem ipsum dolor sit amet',
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ListView.separated(
+              shrinkWrap: true,
+              itemCount: 5,
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 20);
+              },
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                              height: 200,
+                              width: 150,
+                              decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(beach),
+                                      fit: BoxFit.cover))),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 15.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text(
+                                  'Friday | 18 AUG',
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(
+                                      color: Colors.grey.shade800,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu nibh nec augue blandit condimentum sed sed turpis. Quisque suscipit orci non velit tempus, eget congue leo tincidunt. ',
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(
+                                    color: Colors.grey.shade800,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 25,
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        'View',
+                                        style: TextStyle(
+                                          color: Colors.grey.shade800,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(
+                                              Icons.arrow_forward_ios_rounded,
+                                              size: 15)),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ],
         ),
